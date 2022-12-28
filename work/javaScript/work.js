0e1bcb73d5a21b270bf8924f956417c9eb6e1681
shingo05 <nishidash.vntn01@gmail.com>
2022-12-28 21:55:55 +0900

    work

--- work/javaScript/work.js
+++ work/javaScript/work.js
@@ -1,39 +1,34 @@
 'use strict'; /* 厳格にエラーをチェック */
 {
-    const tabs = document.querySelectorAll('.tab-name-item');
-    console.log(tabs);
+    window.addEventListener('DOMContentLoaded', function(){
+  //name 属性が categories の input 要素（ラジオボタン）の集まり（静的な NodeList）を取得
+  const input_categories = document.querySelectorAll("input[name=categories]");
+  //全ての .target の要素（target クラスを指定された div 要素）を取得
+  const targets = document.querySelectorAll(".target");
+  
+  //ループで各ラジオボタンにイベントリスナを設定
+  for(let input_category of input_categories) {
+    //change イベントリスナを各ラジオボタンに登録
+    input_category.addEventListener('change',function(){    
+      for(let target of targets) {
+        //全ての .target の要素に display: block; を設定
+        target.style.setProperty('display', 'block');
+      }
+      //ラジオボタンが選択された場合
+      if( this.checked ) {
+        //ラジオボタンの value 属性が All 以外の場合
+        if(this.value !== 'All') {
+          //target クラスの要素で data-category 属性にこのラジオボタンの value 属性の値が含まれていないものを全て取得
+          const not_checked_categories = document.querySelectorAll('.target:not([data-category~=' + '"' + this.value + '"])');
+          //取得した要素に display: none を設定して非表示に
+          for(let not_checked_category of not_checked_categories) {
+            not_checked_category.style.setProperty('display', 'none');
+          }
+        }
+      }
+    });
+  }     
+});
 
-    tabs.forEach((tabs) => {
-        tabs.addEventListener('click', tabSwitch);
-    })
 
-    function tabSwitch(e){
-        const tabTargetData = e.currentTarget.dataset.name;
-
-        const tabList = e.currentTarget.closest('.tab-name');
-        console.log(tabList);
-        const tabItems = tabList.querySelectorAll('.tab-name-item');
-        console.log(tabItems);
-
-        const tabExItems = tabList.nextElementSibling.querySelectorAll('.tab-ex-item');
-        console.log(tabExItems);
-
-        tabItems.forEach((tabItem) => {
-            tabItem.classList.remove('is-active');
-        })
-
-        tabExItems.forEach((tabExItem) => {
-            tabExItem.classList.remove('is-show');
-        })
-
-        e.currentTarget.classList.add('is-active');
-
-        tabExItems.forEach((tabExItem) => {
-            if(tabExItem.dataset.ex === tabTargetData){
-                tabExItem.classList.add('is-show');
-            }
-        })
-    }
-
-    
 }
