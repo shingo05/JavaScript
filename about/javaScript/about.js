3f4f5153e01d34cae07c20f1db0bd6173e6be8bf
tolbou <ttorubou@gmail.com>
2023-01-29 18:34:20 +0900

    roading

--- about/javaScript/about.js
+++ about/javaScript/about.js
@@ -1,3 +1,25 @@
 setTimeout(function () {
     $('#loading-bg').fadeOut();
 }, 3000);
+
+
+// // Get the image element
+// var image = document.getElementById("../img/about1.jpg");
+
+// // Create an array of image sources
+// var imageSources = ["../img/about2.jpg", "about1.jpg"];
+
+// // Create a counter
+// var counter = 0;
+
+// // Create a function that changes the image source
+// function changeImage() {
+//     image.src = imageSources[counter];
+//     counter++;
+//     if (counter >= imageSources.length) {
+//         counter = 0;
+//     }
+// }
+
+// // Set the function to run every 1000 milliseconds (1 second)
+// setInterval(changeImage, 1000);
